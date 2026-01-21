e62e18afdcf3af7c703f8872acbc0111cd88cab0
Nico Linares Mayer <nlinares@payway.com.ar>
2026-01-21 19:37:25 -0300

    qwdqw

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  output: 'export', 
 };
 
 export default nextConfig;
