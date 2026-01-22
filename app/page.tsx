06056e5936692f7ba44afd11aab92fb30da9b797
Nico Linares Mayer <nlinares@payway.com.ar>
2026-01-21 22:41:20 -0300

    commitment

--- app/page.tsx
+++ app/page.tsx
@@ -49,20 +49,26 @@ export default function Landing() {
                     </h1>
                 </div>
 
-                      <motion.svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 20"
-    stroke="white"
-    className="w-6 h-6 mt-9"
-    animate={{ x: [5, 10, 5], rotate: [90, 90, 90] }} // mueve y rota suavemente
-    transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop", ease: "easeInOut" }}
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
-  </motion.svg>
+                <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" stroke="white" className="w-6 h-6 mt-9" animate={{
+                        x: [
+                            5, 10, 5
+                        ],
+                        rotate: [90, 90, 90]
+                    }}
+                    // mueve y rota suavemente
+                    transition={{
+                        duration: 1.5,
+                        repeat: Infinity,
+                        repeatType: "loop",
+                        ease: "easeInOut"
+                    }}>
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M19 9l-7 7-7-7"/>
+                </motion.svg>
             </motion.a>
-
-            
 
         </div>
     )
