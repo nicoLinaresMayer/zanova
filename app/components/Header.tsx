285966abe0d0976a023afb3e5bac2bf57522c47d
Nico Linares Mayer <nlinares@payway.com.ar>
2026-02-02 17:39:57 -0300

    version estable

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
   return (
     <motion.header
       style={{ opacity }}
-      className="fixed top-0 left-0 w-full bg-white p-4 z-50 bg-gradient-to-b from-black/15 to-transparent pointer-events-none"
+      className="fixed top-0 left-0 w-full bg-pearl p-4 z-50 bg-gradient-to-b from-black/15 to-transparent pointer-events-none"
     >
-      <h1 className="text-3xl tracking-widest text-center font-zanova text-light-black">Zanova</h1>
+      <h1 className="text-3xl md:text-5xl tracking-widest text-center font-zanova text-light-black">Zanova</h1>
     </motion.header>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const INSTAGRAM_URL = 'https://www.instagram.com/zanova.zn/'
 export default function Landing() {
     return (
         <div
-            className="fixed inset-0 z-10 flex items-center justify-centerflex flex-col min-h-[100dvh]  text-pearl">
+            className="fixed inset-0 z-10 flex items-center justify-centerflex flex-col min-h-[100dvh] text-pearl ">
                 <div className="absolute top-[20%]">
             <motion.a
                 href={INSTAGRAM_URL}
@@ -18,10 +18,10 @@ export default function Landing() {
                 }}
                 className="">
                 <div className="flex flex-col">
-                    <h2 className="text-1.5xl font-times text-center">
+                    <h2 className="text-1.5xl md:text-4xl font-times text-center">
                         Proximamente
                     </h2>
-                    <h1 className="text-2xl font-bold text-center font-zanova">Tienda Zanova</h1>
+                    <h1 className="text-2xl md:text-4xl font-bold text-center font-zanova md:mb-10">Tienda Zanova</h1>
 
                 </div>
 
@@ -41,16 +41,11 @@ export default function Landing() {
                 <div className="flex flex-col">
 
                     <h1
-                        className="text-3xl f text-center font-ig  drop-shadow-lg flex flex-col items-center">
-                        <span className="text-1.5xl font-times drop-shadow-lg -mb-2 mt-[clamp(1rem,1vh,2rem)]">
+                        className="text-3xl md:text-8xl f text-center font-ig  drop-shadow-lg flex flex-col items-center">
+                        <span className="text-1.5xl md:text-7xl font-times drop-shadow-lg -mb-2 md:-mb-3 mt-[clamp(1rem,1vh,2rem)]">
                             Visitanos en
                         </span>
-                Instagram
-
-                    </h1>
-                </div>
-
-                <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" stroke="white" className="w-6 h-6 mt-9" animate={{
+                <div className = 'flex'>Instagram <motion.svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 20" stroke="white" className="w-5 md:w-12 h-5 md:h-12 mt-3 md:mt-10" animate={{
                         x: [
                             5, 10, 5
                         ],
@@ -68,7 +63,13 @@ export default function Landing() {
                         strokeLinejoin="round"
                         strokeWidth={2}
                         d="M19 9l-7 7-7-7"/>
-                </motion.svg>
+                </motion.svg></div>
+
+                    </h1>
+                    
+                </div>
+
+               
             </motion.a>
                     </div>
         </div>
