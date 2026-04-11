a34c6d8b027c6bc4ab2731586aa8890807d5c2b0
nLinaresMayer <nlinares@payway.com.ar>
2026-04-11 18:56:58 -0300

    fix metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import HeroImage from './components/HeroImage'
 import ProductsFetch from './components/ProductsFetch'
 
-export const metadata = {
-  title: 'Zanova - Diseños exclusivos',
-  description: 'Tienda oficial de Zanova'
-}
-
 export default function LandingPage() {
   return (
     <main className="relative z-10 text-white">
